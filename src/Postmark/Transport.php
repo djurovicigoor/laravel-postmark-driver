2beb75295ecf06a2162bf5f0d6ab59a66ea48712
Djurovic Igor <djurovicigoor@users.noreply.github.com>
2021-08-06 22:24:34 +0200

    Added message id support

--- src/Postmark/Transport.php
+++ src/Postmark/Transport.php
@@ -107,6 +107,12 @@ class Transport implements Swift_Transport {
 		if ($responseEvent = $this->_eventDispatcher->createResponseEvent($this, $response->getBody()->__toString(), $success)) {
 			$this->_eventDispatcher->dispatchEvent($responseEvent, 'responseReceived');
 		}
+		
+		// Get the Postmark message ID
+        	$jsonResponse = json_decode($response->getBody()->__toString(), true);
+        	if (isset($jsonResponse['MessageID'])) {
+            		$message->getHeaders()->addTextHeader('X-PM-Message-Id', $jsonResponse['MessageID']);
+        	}
 
 		if ($sendEvent) {
 			$sendEvent->setResult($success ? \Swift_Events_SendEvent::RESULT_SUCCESS : \Swift_Events_SendEvent::RESULT_FAILED);
